a0c87633883d8a6eb4374cbbb8d55e60315314ff
simon987 <me@simon987.net>
2019-12-27 16:04:59 -0500

    bugfix

--- fastimagehash.go
+++ fastimagehash.go
@@ -155,7 +155,13 @@ func PHashFile(filepath string, hashSize, highFreqFactor int) (*Hash, Code) {
 
 func PHashMem(buf []byte, hashSize, highFreqFactor int) (*Hash, Code) {
 	var ret C.int
-	hash := C.phash_mem_wr(unsafe.Pointer(&buf[0]), C.size_t(len(buf)), C.int(hashSize), C.int(highFreqFactor), &ret)
+
+	p := C.malloc(C.size_t(len(buf)))
+	cBuf := (*[1 << 30]byte)(p)
+	copy(cBuf[:], buf)
+
+	hash := C.phash_mem_wr(p, C.size_t(len(buf)), C.int(hashSize), C.int(highFreqFactor), &ret)
+	C.free(p)
 	return retHash(hash, hashSize, ret)
 }
 
